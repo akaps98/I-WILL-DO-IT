d687b86d8a34a00e88ffd519956b51de3356f5b2
Tony <fc0302@naver.com>
2024-12-10 15:51:18 +0700

    apply login css

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "./LogIn.css";
 
 const LogIn = () => {
     return (
-        <div>
-            <p>LogIn</p>
+        <div className="login-container">
+        <div className="login-box">
+          <h1>Log In</h1>
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" placeholder="Enter your password" />
+            </div>
+            <button type="submit" className="login-button">Log In</button>
+          </form>
         </div>
+      </div>
     );
 }
 
